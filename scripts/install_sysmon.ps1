cf2225211d49b16c90cf00258e8e0f817eddfb38
Tony M Lambert <ForensicITGuy@users.noreply.github.com>
2024-11-16 19:38:11 -0600

    Update install_sysmon.ps1 to use sysmon-modular config

--- scripts/install_sysmon.ps1
+++ scripts/install_sysmon.ps1
@@ -1,8 +1,7 @@
 ﻿# Curl and unzip sysmon off the windows sysinternals page
 curl https://download.sysinternals.com/files/Sysmon.zip -OutFile sysmon.zip
 Expand-Archive sysmon.zip
-# Curl and unzip the swift on config xml
-curl https://github.com/SwiftOnSecurity/sysmon-config/archive/refs/heads/master.zip -OutFile sysmon-config.zip
-Expand-Archive sysmon-config.zip
+# Curl to retrieve the sysmon-modular default config
+curl https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml -OutFile sysmonconfig.xml
 # install sysmon 
-.\sysmon\sysmon -accepteula -i .\sysmon-config\sysmon-config-master\sysmonconfig-export.xml
+.\sysmon\sysmon -accepteula -i .\sysmonconfig.xml
